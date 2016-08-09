48b8484b29f8d604182b71879069ff6bf63f952c
lonesailor <sujal.paudel8@gmail.com>
2016-08-09 11:29:11 -0500

    First commit

---
+++ js/service.js
@@ -0,0 +1,37 @@
+angular.module('appShop').service('itemListService', function(){
+   this.itemList = {['TV', 'Cell Phones', 'Laptops', 'Tablets', 'Mice', 'Guitars']};
+    this.items = [
+        {
+            'itemType':'',
+            'itemName':'',
+            'itemPrice':'',
+            'itemImage':'',
+            'itemDesc':''
+        }
+        
+        {
+            'itemType':'',
+            'itemName':'',
+            'itemPrice':'',
+            'itemImage':'',
+            'itemDesc':''
+        }
+        
+        {
+            'itemType':'',
+            'itemName':'',
+            'itemPrice':'',
+            'itemImage':'',
+            'itemDesc':''
+        }
+        
+        {
+            'itemType':'',
+            'itemName':'',
+            'itemPrice':'',
+            'itemImage':'',
+            'itemDesc':''
+        }
+        
+    ];
+});
